7ca87b73a4ebd2bb2f07ac0841aee917f3a2b32b
mjahanzeb <malikjahanzeb7@gmail.com>
2014-11-13 20:18:23 -0500

    Communards and About working

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,7 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
 //= require turbolinks
-//= require_tree .
+//= require bootstrap/jquery.min
+//= require bootstrap/bootstrap.min
+//= require slick/slick.min
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Nakedpunch::Application.routes.draw do
   root  'static_pages#home'
-  get "static_pages/communards"
-  get "static_pages/about"
+  match '/communards',  to: 'static_pages#communards',  via: 'get'
+  match '/about',       to: 'static_pages#about',       via: 'get'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
